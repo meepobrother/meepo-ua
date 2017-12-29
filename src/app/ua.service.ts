3332c5cdd29970cb6f150044e098cb972862bdce
15138730370 <1037483576@qq.com>
2017-12-29 10:56:20 +0800

    save to back

--- src/app/ua.service.ts
+++ src/app/ua.service.ts
@@ -6,9 +6,16 @@ var parser = require('ua-parser-js');
 export class UaService {
     ua: any = navigator.userAgent.toLowerCase();
     constructor() { }
+    //是否ios
+    isIos() {
+        return !!this.ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    }
+    //是否android
+    isAndroid() {
+        return this.ua.indexOf('Android') > -1 || this.ua.indexOf('Adr') > -1;
+    }
     // 是否微信
     isWechat() {
-        let ua = this.ua.ua.toLowerCase();
-        return ua.match(/MicroMessenger/i) == 'micromessenger'
+        return this.ua.match(/MicroMessenger/i) == 'micromessenger'
     }
 }
